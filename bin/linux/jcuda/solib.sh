96c60882d60a259b8f57697ccf3bc1eff75c556e
Mike Schneiders <schnied@bme-venus.bme.utexas.edu>
2010-03-02 22:00:19 -0600

    compiled the 64-bit linux libcudafft.so on Pengyu's bme-earth

--- bin/linux/jcuda/solib.sh
+++ bin/linux/jcuda/solib.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 rm libcudafft.so
+
+export PATH="/usr/local/cuda/bin:$PATH"
+
 nvcc -Xcompiler -fPIC --shared ../../macosx/jcuda/convolution.cu -o libcudafft.so -I../../macosx/jcuda -I/home/schnied/Software/jdk1.6.0_17/include -I/home/schnied/Software/jdk1.6.0_17/include/linux -I/home/schnied/NVIDIA_GPU_Computing_SDK/C/common/inc -L/usr/local/cuda/lib -lcudart -lcuda -lcufft
