8d6df73abc29629d983c81d2a65a59b131ad1f5d
Michael J. Schnieders <michael.schnieders@gmail.com>
2011-01-06 17:00:44 -0800

    changed dates from 2010 to 2011, added a developer section to the website

--- modules/ui/src/main/java/ffx/ui/MainPanel.java
+++ modules/ui/src/main/java/ffx/ui/MainPanel.java
@@ -1,7 +1,7 @@
 /**
  * Title: Force Field X
  * Description: Force Field X - Software for Molecular Biophysics.
- ** Copyright: Copyright (c) Michael J. Schnieders 2001-2010
+ ** Copyright: Copyright (c) Michael J. Schnieders 2001-2011
  *
  * This file is part of Force Field X.
  *
