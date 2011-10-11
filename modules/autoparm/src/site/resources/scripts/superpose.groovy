07e63a1074c2a6dd1c221a66d762ee596ecc254b
Michael J. Schnieders <michael.schnieders@gmail.com>
2011-10-10 21:34:07 -0500

    removed the ffx/scripts directory and modules website scripts directories from the git repo - we only need to keep the src/main/groovy copy in git

--- modules/autoparm/src/site/resources/scripts/superpose.groovy
+++
@@ -1,7 +0,0 @@
-//SUPERPOSE
-//Give two xyzfiles
-//Program will print out information about the distance between the two
-String fname1 = args[0];
-String fname2 = args[1];
-
-superpose(fname1,fname2);
