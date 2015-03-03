04927f719b1f1574f01119dce52298d8c0cd9656
Michael J. Schnieders <michael-schnieders@uiowa.edu>
2015-03-03 16:24:43 -0600

    updated Linux OpenCL build script

--- modules/numerics/src/main/java/ffx/numerics/fft/CLFFT_build.sh
+++ modules/numerics/src/main/java/ffx/numerics/fft/CLFFT_build.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
-
-export JAVA_HOME="/Dedicated/schnieders/programs/jdk1.8.0_11"
-export CLFFT_HOME="/Dedicated/schnieders/programs/clMath/clFFT-2.2.0"
+export JAVA_HOME="/Dedicated/schnieders/programs/jdk1.8.0_31"
+export CLFFT_HOME="/Dedicated/schnieders/programs/clMath/clFFT-current/src"
 export OPENCL_HOME="/opt/intel/opencl-1.2-3.2.1.16712"
 
 icc CLFFT_Impl.cpp -shared -fPIC -o libJclFFT.so -I$JAVA_HOME/include -I$JAVA_HOME/include/linux -I$CLFFT_HOME/include -I$OPENCL_HOME/include
