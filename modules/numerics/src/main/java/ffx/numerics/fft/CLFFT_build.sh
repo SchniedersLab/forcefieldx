09e8c7d72d26fcd9b11550ce875ff33979141dce
Michael J. Schnieders <michael-schnieders@uiowa.edu>
2015-03-04 15:48:27 -0600

    updated OpenCL run scripts for Neon; added a script for running CUDA FFT on Neon

--- modules/numerics/src/main/java/ffx/numerics/fft/CLFFT_build.sh
+++ modules/numerics/src/main/java/ffx/numerics/fft/CLFFT_build.sh
@@ -1,6 +1,10 @@
 #! /bin/bash
 export JAVA_HOME="/Dedicated/schnieders/programs/jdk1.8.0_31"
-export CLFFT_HOME="/Dedicated/schnieders/programs/clMath/clFFT-current/src"
-export OPENCL_HOME="/opt/intel/opencl-1.2-3.2.1.16712"
 
-icc CLFFT_Impl.cpp -shared -fPIC -o libJclFFT.so -I$JAVA_HOME/include -I$JAVA_HOME/include/linux -I$CLFFT_HOME/include -I$OPENCL_HOME/include
+export CLFFT_HOME="/Dedicated/schnieders/programs/clMath/clFFT-nVidia/src"
+export OPENCL_HOME="/opt/local/cuda"
+
+#export CLFFT_HOME="/Dedicated/schnieders/programs/clMath/clFFT-current/src"
+#export OPENCL_HOME="/opt/intel/opencl-1.2-3.2.1.16712"
+
+gcc CLFFT_Impl.cpp -shared -fPIC -o libJclFFT.so -I$JAVA_HOME/include -I$JAVA_HOME/include/linux -I$CLFFT_HOME/include -I$OPENCL_HOME/include
