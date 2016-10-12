206feb4f586924cff59332ce834397d059bb7110
Michael Schnieders <michael-schnieders@uiowa.edu>
2016-10-12 17:03:10 -0500

    updated to the PairwiseSchedule to ensure all threads have work

--- modules/potential/src/main/java/ffx/potential/nonbonded/PairwiseSchedule.java
+++ modules/potential/src/main/java/ffx/potential/nonbonded/PairwiseSchedule.java
@@ -54,6 +54,7 @@ public class PairwiseSchedule extends IntegerSchedule {
     private static final Logger logger = Logger.getLogger(PairwiseSchedule.class.getName());
     private int nAtoms;
     private final int nThreads;
+    private int offset = 0;
     private final Range ranges[];
     private final boolean threadDone[];
 
@@ -121,7 +122,7 @@ public class PairwiseSchedule extends IntegerSchedule {
      */
     public void updateRanges(int totalInteractions, int listCount[]) {
         int id = 0;
-        int goal = totalInteractions / nThreads;
+        int goal = totalInteractions / (nThreads + offset);
         int num = 0;
         int start = 0;
         for (int i = 0; i < nAtoms; i++) {
@@ -145,23 +146,29 @@ public class PairwiseSchedule extends IntegerSchedule {
                 start = i + 1;
 
                 /**
-                 * Out of atoms. Threads remaining get a null range.
+                 * Out of atoms; Remaining threads would get a null range.
                  */
                 if (start == nAtoms) {
-                    for (int j = id; j < nThreads; j++) {
-                        ranges[j] = null;
-                    }
+                    offset++;
+                    updateRanges(totalInteractions, listCount);
                     break;
                 }
             } else if (i == nAtoms - 1) {
-                /**
-                 * Last atom without reaching goal for current thread.
-                 */
-                ranges[id] = new Range(start, nAtoms - 1);
-                for (int j = id + 1; j < nThreads; j++) {
-                    ranges[j] = null;
+                if (id == nThreads - 1) {
+                    ranges[id] = new Range(start, nAtoms - 1);
+                    break;
                 }
+                /**
+                 * There are threads left with no work.
+                 */
+                offset++;
+                updateRanges(totalInteractions, listCount);
+                break;
             }
         }
+        /**
+         * Reset the offset to zero.
+         */
+        offset = 0;
     }
 }
