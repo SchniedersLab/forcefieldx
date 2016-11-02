4dd8a25a350d7d3e08f85ef2dbbdd70f6c06f93f
Jacob Litman <jacob-litman@kenai.com>
2016-11-02 14:01:07 -0500

    Transition-tempered OSRW can now be seeded with multiple conformations; use -dw AUTO to pick up [file]_[number] (1-indexed) files in current working directory, else use -dw reslist (same syntax as rotamer optimization -lR) to generate an ensemble of side-chain conformations to pull from. As part of this: rotamer optimization can now be set not to print out files (use setPrintFiles()), and the ensemble (as a set of ResidueState[]) can be obtained from the RotamerOptimization object. Additionally, ObjectPair<T, S extends comparable> added to Utilities package so non-comparable objects can be sorted with respect to an external variable. Finally: minor improvements to testLambdaGradient

--- modules/algorithms/src/main/groovy/ffx/algorithms/ttOSRW.groovy
+++ modules/algorithms/src/main/groovy/ffx/algorithms/ttOSRW.groovy
@@ -58,11 +58,16 @@ import ffx.algorithms.MolecularDynamics;
 import ffx.algorithms.TransitionTemperedOSRW;
 import ffx.algorithms.Integrator.Integrators;
 import ffx.algorithms.Thermostat.Thermostats;
+import ffx.algorithms.RotamerOptimization;
 import ffx.potential.QuadTopologyEnergy;
 import ffx.potential.DualTopologyEnergy;
 import ffx.potential.ForceFieldEnergy;
 import ffx.potential.bonded.Atom;
+import ffx.potential.bonded.Residue;
+import ffx.potential.bonded.Polymer;
+import ffx.potential.bonded.RotamerLibrary;
 import ffx.potential.MolecularAssembly;
+import ffx.numerics.Potential;
 
 // Asychronous communication between walkers.
 boolean asynchronous = false;
@@ -177,6 +182,10 @@ double temperingParam = 2.0;
 // Write traversal snapshots
 boolean writeTraversals = false;
 
+//@Field String distributeWalkers = "";
+@Field def distResidues = [];
+@Field boolean autoDist = false;
+
 int numParallel = 1;
 @Field int threadsAvail = edu.rit.pj.ParallelTeam.getDefaultThreadCount();
 @Field int threadsPer = threadsAvail;
@@ -224,6 +233,7 @@ cli.rn(longOpt:'resetNumSteps', args:1, argName:'true', 'Ignore prior steps logg
 cli.uaA(longOpt:'unsharedAtomsA', args:1, argName:'None', 'Quad-Topology: Period-separated ranges of A dual-topology atoms not shared by B');
 cli.uaB(longOpt:'unsharedAtomsB', args:1, argName:'None', 'Quad-Topology: Period-separated ranges of B dual-topology atoms not shared by A');
 cli.np(longOpt:'numParallel', args:1, argName:'1', 'Number of topology energies to calculate in parallel');
+cli.dw(longOpt:'distributeWalkers', args:1, argName:'false', 'AUTO: Pick up per-walker configurations as [filename.pdb]_[num], or specify a residue to distribute on.');
 
 def options = cli.parse(args);
 List<String> arguments = options.arguments();
@@ -391,6 +401,14 @@ if (options.tp) {
     temperingParam = Double.parseDouble(options.tp);
 }
 
+if (options.dw) {
+    if (options.dw.equalsIgnoreCase("AUTO")) {
+        autoDist = true;
+    } else {
+        distResidues = options.dw.split("\\.");
+    }
+}
+
 if (options.np) {
     numParallel = Integer.parseInt(options.np);
     if (threadsAvail % numParallel != 0) {
@@ -480,10 +498,40 @@ if (arguments.size() >= 2) {
 
 @Field Pattern rangeregex = Pattern.compile("([0-9]+)-?([0-9]+)?");
 
+// Apply the command line lambda value if a lambda restart file does not exist.
+if (!lambdaRestart.exists()) {
+    if (lambda < 0.0 || lambda > 1.0) {
+        if (size > 1) {
+            dL = 1.0 / (size - 1.0);
+            lambda = rank * dL;
+            if (lambda > 1.0) {
+                lambda = 1.0;
+            }
+            if (lambda < 0.0) {
+                lambda = 0.0;
+            }
+            logger.info(String.format(" Setting lambda to %5.3f.", lambda));
+        } else {
+            lambda = 0.5;
+            logger.info(String.format(" Setting lambda to %5.3f", lambda));
+        }
+    }
+}
+
 /**
  * Handles opening a file (filename), with 0-indexed number topNum.
  */
 private void openFile(String toOpen, File structFile, int topNum) {
+    if (autoDist) {
+        int rank = world.rank();
+        String openName = String.format("%s_%d", toOpen, rank+1);
+        File testFile = new File(openName);
+        if (testFile.exists()) {
+            toOpen = openName;
+        } else {
+            logger.warning(String.format(" File %s does not exist; using default %s", openName, toOpen));
+        }
+    }
     open(toOpen, threadsPer);
     if (size > 1) {
         active.setFile(structFile);
@@ -652,7 +700,85 @@ TransitionTemperedOSRW osrw = null;
 // Save a reference to the first topology.
 //topologies[0] = active;
 
+void optStructure(MolecularAssembly mola, Potential pot) {
+    if (!distResidues) {
+        throw new IllegalArgumentException(" Programming error: Must have list of residues to split on!");
+    }
+    
+    def resList = [];
+    Polymer[] polymers = mola.getChains();
+    
+    def nochainMatcher = ~/^\ ?([0-9]+)$/;
+    def chainMatcher = ~/^([a-zA-Z])([0-9]+)$/;
+    
+    for (String ts : distResidues) {
+        Character chainID = 'A';
+        def m = chainMatcher.matcher(ts);
+        int resNum = -1;
+        if (m.find()) {
+            chainID = m.group(1);
+            resNum = Integer.parseInt(m.group(2));
+        } else {
+            m = nochainMatcher.matcher(ts);
+            if (m.find()) {
+                resNum = Integer.parseInt(m.group(1));
+            } else {
+                logger.warning(String.format(" Could not parse %s as a valid residue!", ts));
+                continue;
+            }
+        }
+        
+        logger.info(String.format(" Looking for chain %c residue %d", chainID, resNum));
+        
+        for (Polymer p : mola.getChains()) {
+            if (p.getChainID() == chainID) {
+                for (Residue r : p.getResidues()) {
+                    if (r.getResidueNumber() == resNum && r.getRotamers() != null) {
+                        resList.add(r);
+                    }
+                }
+            }
+        }
+    }
+    
+    if (!resList) {
+        throw new IllegalArgumentException(" No valid entries for distWalkers!");
+    }
+    
+    RotamerOptimization ropt = new RotamerOptimization(mola, pot, sh);
+    
+    ropt.setThreeBodyEnergy(false);
+    ropt.setVerboseEnergies(true);
+    ropt.setEnsemble(size);
+    ropt.setPrintFiles(false);
+    def addedResList = ropt.setResiduesIgnoreNull(resList);
+    
+    RotamerLibrary.setLibrary(RotamerLibrary.ProteinLibrary.Richardson);
+    RotamerLibrary.setUseOrigCoordsRotamer(false);
+    RotamerLibrary.measureRotamers(resList, false);
+    
+    String oldLazyMat = System.getProperty("ro-lazyMatrix");
+    System.setProperty("ro-lazyMatrix", "true");
+    
+    ropt.optimize(RotamerOptimization.Algorithm.GLOBAL_DEE);
+    ropt.setCoordinatesToEnsemble(world.rank());
+    
+    // One final energy call to ensure the coordinates are properly set at the
+    // end of rotamer optimization.
+    double[] xyz = new double[pot.getNumberOfVariables()];
+    pot.getCoordinates(xyz);
+    logger.info(" Final Optimized Energy:");
+    pot.energy(xyz, true);
+    
+    if (oldLazyMat) {
+        System.setProperty("ro-lazyMatrix", oldLazyMat);
+    } else {
+        System.clearProperty("ro-lazyMatrix");
+    }
+}
+
 if (arguments.size() == 1) {
+    energies[0].setLambda(lambda);
     // Check for constant pressure
     if (NPT) {
         //        // Create a barostat.
@@ -679,6 +805,9 @@ if (arguments.size() == 1) {
         if (writeTraversals) {
             osrw.setTraversalOutput(lambdaOneFile, topologies[0], lambdaZeroFile, topologies[0]);
         }
+    }
+    if (distResidues) {
+        optStructure(topologies[0], energies[0]);
     }
 } else if (arguments.size() == 2) {
     // Open the 2nd topology.
@@ -751,6 +880,17 @@ if (arguments.size() == 1) {
     if (numParallel == 2) {
         dualTopologyEnergy.setParallel(true);
     }
+    dualTopologyEnergy.setLambda(lambda);
+    if (distResidues) {
+        if (dualTopologyEnergy.getNumberSharedVariables() == dualTopologyEnergy.getNumberOfVariables()) {
+            logger.info(" Generating starting structure based on dual-topology:");
+            optStructure(topologies[0], dualTopologyEnergy);
+        } else {
+            logger.info(" Generating separate starting structures for each topology of the dual toplogy:");
+            optStructure(topologies[0], energies[0]);
+            optStructure(topologies[1], energies[1]);
+        }
+    }
     // Wrap the DualTopology potential energy inside an OSRW instance.
     osrw = new TransitionTemperedOSRW(dualTopologyEnergy, dualTopologyEnergy, lambdaRestart,
         histogramRestart, active.getProperties(), temperature, timeStep, printInterval,
@@ -760,10 +900,10 @@ if (arguments.size() == 1) {
         osrw.setTraversalOutput(lambdaOneFile, topologies[0], lambdaZeroFile, topologies[1]);
     }
 } else if (arguments.size() == 4) {
-    logger.info(" For quad-topology calculations, systems should be set up as follows:");
+    /*logger.info(" For quad-topology calculations, systems should be set up as follows:");
     logger.info(" The first two define the first dual topology, the second two should define the second dual topology in the same order.");
     logger.info(" For example, for a dual force field correction on decharging sodium, topologies should be in this order:");
-    logger.info(" Sodium-AMOEBA, sodium-AMBER, decharged Na-AMOEBA, decharged Na-AMBER");
+    logger.info(" Sodium-AMOEBA, sodium-AMBER, decharged Na-AMOEBA, decharged Na-AMBER");*/
     
     openFile(arguments.get(1), structureFile, 1);
     openFile(arguments.get(2), structureFile, 2);
@@ -771,6 +911,15 @@ if (arguments.size() == 1) {
     DualTopologyEnergy dtA = new DualTopologyEnergy(topologies[0], topologies[1]);
     // Intentionally reversed order.
     DualTopologyEnergy dtB = new DualTopologyEnergy(topologies[3], topologies[2]);
+    dtA.setLambda(lambda);
+    dtB.setLambda(lambda);
+    
+    if (distResidues) {
+        logger.info(" Generating starting structures for each dual-topology of the quad topology:");
+        optStructure(topologies[0], dtA);
+        optStructure(topologies[3], dtB);
+    }
+    
     List<Integer> uniqueA = new ArrayList<>();
     List<Integer> uniqueB = new ArrayList<>();
     
@@ -869,29 +1018,9 @@ if (arguments.size() == 1) {
 }
 osrw.setDeltaT(temperingParam);
 
-// Apply the command line lambda value if a lambda restart file does not exist.
 if (!lambdaRestart.exists()) {
-    if (lambda >= 0.0 && lambda <= 1.0) {
-        osrw.setLambda(lambda);
-        logger.info(String.format(" Setting lambda to %5.3f.", lambda));
-    } else {
-        if (size > 1) {
-            dL = 1.0 / (size - 1.0);
-            lambda = rank * dL;
-            if (lambda > 1.0) {
-                lambda = 1.0;
-            }
-            if (lambda < 0.0) {
-                lambda = 0.0;
-            }
-            logger.info(String.format(" Setting lambda to %5.3f.", lambda));
-            osrw.setLambda(lambda);
-        } else {
-            lambda = 0.5;
-            logger.info(String.format(" Setting lambda to %5.3f", lambda));
-            osrw.setLambda(lambda);
-        }
-    }
+    logger.info(String.format(" Setting lambda to %5.3f", lambda));
+    osrw.setLambda(lambda);
 }
 
 // Apply the command line OSRW values if a histogram restart file does not exist.
