714876e105471e10a53420a1865557627494a29e
Michael Schnieders <michael-schnieders@uiowa.edu>
2017-02-08 07:57:46 -0600

    Change TT-OSRW to print out density when doing a local optimization

--- modules/potential/src/main/java/ffx/potential/parsers/XYZFilter.java
+++ modules/potential/src/main/java/ffx/potential/parsers/XYZFilter.java
@@ -46,7 +46,6 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Hashtable;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
