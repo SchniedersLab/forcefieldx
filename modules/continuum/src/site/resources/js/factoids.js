5470046590ec1e228f28e061c9479072a81b0dae
Michael Schnieders <michael-schnieders@uiowa.edu>
2018-01-04 15:26:53 -0600

    remove unused module directories

--- modules/continuum/src/site/resources/js/factoids.js
+++
@@ -1,1 +0,0 @@
-document.write('<iframe src=\'http://www.ohloh.net/p/487436/widgets/project_factoids_stats.html\' scrolling=\'no\' marginHeight=0 marginWidth=0 style=\'height: 250px; width: 370px; border: none;\'><\/iframe>')
