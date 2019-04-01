f8115a0724bad0cd43bf90ff85dc4fe3cad5bbd6
Michael Schnieders <michael-schnieders@uiowa.edu>
2019-04-01 09:03:56 -0500

    Update the ffx.utilities package to use the new FFX license header; delete the BlockAverager class and Groovy script.

--- modules/utilities/src/main/java/ffx/utilities/HashCodeUtil.java
+++ modules/utilities/src/main/java/ffx/utilities/HashCodeUtil.java
@@ -1,47 +1,47 @@
-/**
- * Title: Force Field X.
- * <p>
- * Description: Force Field X - Software for Molecular Biophysics.
- * <p>
- * Copyright: Copyright (c) Michael J. Schnieders 2001-2019.
- * <p>
- * This file is part of Force Field X.
- * <p>
- * Force Field X is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 3 as published by
- * the Free Software Foundation.
- * <p>
- * Force Field X is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
- * details.
- * <p>
- * You should have received a copy of the GNU General Public License along with
- * Force Field X; if not, write to the Free Software Foundation, Inc., 59 Temple
- * Place, Suite 330, Boston, MA 02111-1307 USA
- * <p>
- * Linking this library statically or dynamically with other modules is making a
- * combined work based on this library. Thus, the terms and conditions of the
- * GNU General Public License cover the whole combination.
- * <p>
- * As a special exception, the copyright holders of this library give you
- * permission to link this library with independent modules to produce an
- * executable, regardless of the license terms of these independent modules, and
- * to copy and distribute the resulting executable under terms of your choice,
- * provided that you also meet, for each linked independent module, the terms
- * and conditions of the license of that module. An independent module is a
- * module which is not derived from or based on this library. If you modify this
- * library, you may extend this exception to your version of the library, but
- * you are not obligated to do so. If you do not wish to do so, delete this
- * exception statement from your version.
- */
+//******************************************************************************
+//
+// Title:       Force Field X.
+// Description: Force Field X - Software for Molecular Biophysics.
+// Copyright:   Copyright (c) Michael J. Schnieders 2001-2019.
+//
+// This file is part of Force Field X.
+//
+// Force Field X is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License version 3 as published by
+// the Free Software Foundation.
+//
+// Force Field X is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+// details.
+//
+// You should have received a copy of the GNU General Public License along with
+// Force Field X; if not, write to the Free Software Foundation, Inc., 59 Temple
+// Place, Suite 330, Boston, MA 02111-1307 USA
+//
+// Linking this library statically or dynamically with other modules is making a
+// combined work based on this library. Thus, the terms and conditions of the
+// GNU General Public License cover the whole combination.
+//
+// As a special exception, the copyright holders of this library give you
+// permission to link this library with independent modules to produce an
+// executable, regardless of the license terms of these independent modules, and
+// to copy and distribute the resulting executable under terms of your choice,
+// provided that you also meet, for each linked independent module, the terms
+// and conditions of the license of that module. An independent module is a
+// module which is not derived from or based on this library. If you modify this
+// library, you may extend this exception to your version of the library, but
+// you are not obligated to do so. If you do not wish to do so, delete this
+// exception statement from your version.
+//
+//******************************************************************************
 package ffx.utilities;
 
 import java.lang.reflect.Array;
 
 /**
  * Collected methods which allow easy implementation of <code>hashCode</code>.
- *
+ * <p>
  * Example use case:
  * <pre>
  *  public int hashCode(){
@@ -70,7 +70,7 @@ public final class HashCodeUtil {
     /**
      * booleans.
      *
-     * @param aSeed a int.
+     * @param aSeed    a int.
      * @param aBoolean a boolean.
      * @return a int.
      */
@@ -93,7 +93,7 @@ public final class HashCodeUtil {
      * ints.
      *
      * @param aSeed a int.
-     * @param aInt a int.
+     * @param aInt  a int.
      * @return a int.
      */
     public static int hash(int aSeed, int aInt) {
@@ -119,7 +119,7 @@ public final class HashCodeUtil {
     /**
      * floats.
      *
-     * @param aSeed a int.
+     * @param aSeed  a int.
      * @param aFloat a float.
      * @return a int.
      */
@@ -130,7 +130,7 @@ public final class HashCodeUtil {
     /**
      * doubles.
      *
-     * @param aSeed a int.
+     * @param aSeed   a int.
      * @param aDouble a double.
      * @return a int.
      */
@@ -141,11 +141,11 @@ public final class HashCodeUtil {
     /**
      * <code>aObject</code> is a possibly-null object field, and possibly an
      * array.
-     *
+     * <p>
      * If <code>aObject</code> is an array, then each element may be a primitive
      * or a possibly-null object.
      *
-     * @param aSeed a int.
+     * @param aSeed   a int.
      * @param aObject a {@link java.lang.Object} object.
      * @return a int.
      */
--- modules/utilities/src/main/java/ffx/utilities/Keyword.java
+++ modules/utilities/src/main/java/ffx/utilities/Keyword.java
@@ -1,40 +1,40 @@
-/**
- * Title: Force Field X.
- * <p>
- * Description: Force Field X - Software for Molecular Biophysics.
- * <p>
- * Copyright: Copyright (c) Michael J. Schnieders 2001-2019.
- * <p>
- * This file is part of Force Field X.
- * <p>
- * Force Field X is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 3 as published by
- * the Free Software Foundation.
- * <p>
- * Force Field X is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
- * details.
- * <p>
- * You should have received a copy of the GNU General Public License along with
- * Force Field X; if not, write to the Free Software Foundation, Inc., 59 Temple
- * Place, Suite 330, Boston, MA 02111-1307 USA
- * <p>
- * Linking this library statically or dynamically with other modules is making a
- * combined work based on this library. Thus, the terms and conditions of the
- * GNU General Public License cover the whole combination.
- * <p>
- * As a special exception, the copyright holders of this library give you
- * permission to link this library with independent modules to produce an
- * executable, regardless of the license terms of these independent modules, and
- * to copy and distribute the resulting executable under terms of your choice,
- * provided that you also meet, for each linked independent module, the terms
- * and conditions of the license of that module. An independent module is a
- * module which is not derived from or based on this library. If you modify this
- * library, you may extend this exception to your version of the library, but
- * you are not obligated to do so. If you do not wish to do so, delete this
- * exception statement from your version.
- */
+//******************************************************************************
+//
+// Title:       Force Field X.
+// Description: Force Field X - Software for Molecular Biophysics.
+// Copyright:   Copyright (c) Michael J. Schnieders 2001-2019.
+//
+// This file is part of Force Field X.
+//
+// Force Field X is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License version 3 as published by
+// the Free Software Foundation.
+//
+// Force Field X is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+// details.
+//
+// You should have received a copy of the GNU General Public License along with
+// Force Field X; if not, write to the Free Software Foundation, Inc., 59 Temple
+// Place, Suite 330, Boston, MA 02111-1307 USA
+//
+// Linking this library statically or dynamically with other modules is making a
+// combined work based on this library. Thus, the terms and conditions of the
+// GNU General Public License cover the whole combination.
+//
+// As a special exception, the copyright holders of this library give you
+// permission to link this library with independent modules to produce an
+// executable, regardless of the license terms of these independent modules, and
+// to copy and distribute the resulting executable under terms of your choice,
+// provided that you also meet, for each linked independent module, the terms
+// and conditions of the license of that module. An independent module is a
+// module which is not derived from or based on this library. If you modify this
+// library, you may extend this exception to your version of the library, but
+// you are not obligated to do so. If you do not wish to do so, delete this
+// exception statement from your version.
+//
+//******************************************************************************
 package ffx.utilities;
 
 import java.io.File;
@@ -62,8 +62,8 @@ import org.apache.commons.io.FilenameUtils;
 public class Keyword {
 
     private static final Logger logger = Logger.getLogger(Keyword.class.getName());
-    private String keyword = null;
-    private Vector<String> data = null;
+    private String keyword;
+    private Vector<String> data;
 
     /**
      * <p>
@@ -80,7 +80,7 @@ public class Keyword {
      * <p>
      * Constructor for Keyword.</p>
      *
-     * @param k a {@link java.lang.String} object.
+     * @param k     a {@link java.lang.String} object.
      * @param entry a {@link java.lang.String} object.
      */
     public Keyword(String k, String entry) {
@@ -92,10 +92,10 @@ public class Keyword {
      * <p>
      * Constructor for Keyword.</p>
      *
-     * @param k a {@link java.lang.String} object.
+     * @param k     a {@link java.lang.String} object.
      * @param entry an array of {@link java.lang.String} objects.
      */
-    public Keyword(String k, String entry[]) {
+    public Keyword(String k, String[] entry) {
         this(k);
         data.addAll(Arrays.asList(entry));
     }
@@ -116,7 +116,7 @@ public class Keyword {
      *
      * @param entry an array of {@link java.lang.String} objects.
      */
-    public void append(String entry[]) {
+    public void append(String[] entry) {
         data.addAll(Arrays.asList(entry));
     }
 
@@ -167,7 +167,9 @@ public class Keyword {
         logger.info(this.toString());
     }
 
-    /** {@inheritDoc} */
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder(keyword + " ");
@@ -180,45 +182,40 @@ public class Keyword {
     /**
      * This method sets up configuration properties in the following precedence
      * * order:
-     *
+     * <p>
      * 1.) Structure specific properties (for example pdbname.properties)
-     *
+     * <p>
      * 2.) Java system properties a.) -Dkey=value from the Java command line b.)
      * System.setProperty("key","value") within Java code.
-     *
+     * <p>
      * 3.) User specific properties (~/.ffx/ffx.properties)
-     *
+     * <p>
      * 4.) System wide properties (file defined by environment variable
      * FFX_PROPERTIES)
-     *
+     * <p>
      * 5.) Internal force field definition.
      *
-     * @since 1.0
      * @param file a {@link java.io.File} object.
      * @return a {@link org.apache.commons.configuration2.CompositeConfiguration}
      * object.
+     * @since 1.0
      */
     public static CompositeConfiguration loadProperties(File file) {
-        /**
-         * Command line options take precedence.
-         */
+
+        // Command line options take precedence.
         CompositeConfiguration properties = new CompositeConfiguration();
 
-        /**
-         * JVM system properties are read first.
-         *
-         * a.) -Dkey=value from the Java command line
-         *
-         * b.) System.setProperty("key","value") within Java code.
+        /*
+          JVM system properties are read first.
+          a.) -Dkey=value from the Java command line
+          b.) System.setProperty("key","value") within Java code.
          */
         PropertiesConfiguration systemConfiguration = new PropertiesConfiguration();
         systemConfiguration.append(new SystemConfiguration());
         systemConfiguration.setHeader("JVM system properties (i.e. command line -Dkey=value pairs).");
         properties.addConfiguration(systemConfiguration);
 
-        /**
-         * Structure specific options are 2md.
-         */
+        // Structure specific options are 2md.
         if (file != null) {
             String structureBasename = FilenameUtils.removeExtension(file.getAbsolutePath());
             String propertyFilename
@@ -248,9 +245,7 @@ public class Keyword {
             }
         }
 
-        /**
-         * User specific options are 3rd.
-         */
+        // User specific options are 3rd.
         String filename = System.getProperty("user.home") + File.separator + ".ffx/ffx.properties";
         File userPropFile = new File(filename);
         if (userPropFile.exists() && userPropFile.canRead()) {
@@ -270,9 +265,7 @@ public class Keyword {
             }
         }
 
-        /**
-         * System wide options are 2nd to last.
-         */
+        // System wide options are 2nd to last.
         filename = System.getenv("FFX_PROPERTIES");
         if (filename != null) {
             File systemPropFile = new File(filename);
@@ -294,9 +287,7 @@ public class Keyword {
             }
         }
 
-        /**
-         * Echo the interpolated configuration.
-         */
+        // Echo the interpolated configuration.
         if (logger.isLoggable(Level.FINE)) {
             Iterator<String> i = properties.getKeys();
             StringBuilder sb = new StringBuilder();
