7ce58d9192b0dcb96cf2f97af9838ad2ca4c2c3a
Michael Schnieders <michael-schnieders@uiowa.edu>
2020-03-23 18:09:17 -0500

    Remove unused OpenCL FFT code

--- modules/numerics/src/main/java/ffx/numerics/fft/CLFFT_build.mac.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk1.7.0_75.jdk/Contents/Home"
-export CLFFT_HOME="/Data/opencl/clFFT/src"
-export OPENCL_HOME="/Developer/GPU\ Computing/OpenCL/common"
-
-gcc CLFFT_Impl.cpp -shared -fPIC -o libJclFFT.dylib -framework OpenCL -I$JAVA_HOME/include -I$JAVA_HOME/include/darwin -I$CLFFT_HOME/include -I/Developer/GPU\ Computing/OpenCL/common/inc -L$CLFFT_HOME/library -lclFFT
