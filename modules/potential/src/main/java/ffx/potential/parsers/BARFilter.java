982fb6b437325188b8949205262ca0799cb5fa59
rgogal <rgogal@uiowa.edu>
2021-04-08 10:40:16 -0500

    Single .bar file function and start/end snapshot choice

--- modules/potential/src/main/java/ffx/potential/parsers/BARFilter.java
+++ modules/potential/src/main/java/ffx/potential/parsers/BARFilter.java
@@ -36,16 +36,36 @@ public class BARFilter {
     private double[] volume1;
     private double[] volume2;
     private int snaps;
+    private int startingSnap = 0;
+    private int endingSnap = 0;
     private int count = 0;
     private double temp;
+    private boolean getLambda = false;
+
 
 
     /**
      * BARFilter constructor
      * @param barFile a {@link java.util.List} object.
+     *
      */
     public BARFilter(File barFile) {
         this.givenFile = barFile;
+
+    }
+
+    /**
+     * BARFilter constructor
+     * @param barFile a {@link java.util.List} object.
+     * @param startingSnap a {@link java.util.List} object.
+     * @param endingSnap a {@link java.util.List} object.
+     */
+    public BARFilter(File barFile, int startingSnap, int endingSnap) {
+
+        this.givenFile = barFile;
+        this.startingSnap = startingSnap;
+        this.endingSnap = endingSnap;
+
     }
 
     /**
@@ -76,6 +96,8 @@ public class BARFilter {
 
     }
 
+
+
     /**
      * Read TINKER bar files and parse the snapshots into energy arrays
      * @return
@@ -87,6 +109,7 @@ public class BARFilter {
         ArrayList<Double> ens2lam2 = new ArrayList<Double>();
         ArrayList<Double> vol1 = new ArrayList<Double>();
         ArrayList<Double> vol2 = new ArrayList<Double>();
+        int snapshots = 0;
         try (BufferedReader br = new BufferedReader(new FileReader(givenFile))) {
 
             String str = "";
@@ -94,31 +117,67 @@ public class BARFilter {
 
             while ((data = br.readLine()) != null) {
                 count += 1;
-
                 String[] tokens = data.trim().split(" +");
-                if (data.contains(".xyz") || tokens.length < 3) {
-                    snaps = Integer.parseInt(tokens[0]);
-                } else if (count <= snaps + 1 && count != 1) {
-                    if (tokens.length == 4) {
-                        vol1.add(Double.parseDouble(tokens[3]));
+                if(startingSnap != 0 || endingSnap !=0){
+                    if(startingSnap != 0 && endingSnap == 0){
+                        endingSnap = snaps;
+                    }
+                    if (data.contains(".xyz") || tokens.length < 3) {
+                        snaps = Integer.parseInt(tokens[0]);
+                    }
+                    snapshots = (endingSnap - startingSnap) + 1;
+                    if(count >= startingSnap + 1 && count <= endingSnap + 1){
+                        if (tokens.length == 4) {
+                            vol1.add(Double.parseDouble(tokens[3]));
+                        }
+                        ens1lam1.add(Double.parseDouble(tokens[1]));
+                        ens1lam2.add(Double.parseDouble(tokens[2]));
+                    } else if (count >= snaps+startingSnap+2 && count <= snaps + endingSnap + 2 ){
+                        if (tokens.length == 4) {
+                            vol2.add(Double.parseDouble(tokens[3]));
+                        }
+                        ens2lam1.add(Double.parseDouble(tokens[1]));
+                        ens2lam2.add(Double.parseDouble(tokens[2]));
                     }
 
-                    ens1lam1.add(Double.parseDouble(tokens[1]));
-                    ens1lam2.add(Double.parseDouble(tokens[2]));
-                } else if (count > snaps + 2) {
-                    if (tokens.length == 4) {
-                        vol2.add(Double.parseDouble(tokens[3]));
+                } else {
+
+                    if (data.contains(".xyz") || tokens.length < 3) {
+                        snaps = Integer.parseInt(tokens[0]);
+                    } else if (count <= snaps + 1 && count != 1) {
+                        if (tokens.length == 4) {
+                            vol1.add(Double.parseDouble(tokens[3]));
+                        }
+
+                        ens1lam1.add(Double.parseDouble(tokens[1]));
+                        ens1lam2.add(Double.parseDouble(tokens[2]));
+                    } else if (count > snaps + 2) {
+                        if (tokens.length == 4) {
+                            vol2.add(Double.parseDouble(tokens[3]));
+                        }
+                        ens2lam1.add(Double.parseDouble(tokens[1]));
+                        ens2lam2.add(Double.parseDouble(tokens[2]));
                     }
-                    ens2lam1.add(Double.parseDouble(tokens[1]));
-                    ens2lam2.add(Double.parseDouble(tokens[2]));
                 }
+
             }
-            e1l1 = new double[snaps];
-            e1l2 = new double[snaps];
-            e2l1 = new double[snaps];
-            e2l2 = new double[snaps];
-            volume1 = new double[snaps];
-            volume2 = new double[snaps];
+            if(snapshots != 0){
+                e1l1 = new double[snapshots];
+                e1l2 = new double[snapshots];
+                e2l1 = new double[snapshots];
+                e2l2 = new double[snapshots];
+                volume1 = new double[snapshots];
+                volume2 = new double[snapshots];
+                snaps = snapshots;
+            } else {
+                e1l1 = new double[snaps];
+                e1l2 = new double[snaps];
+                e2l1 = new double[snaps];
+                e2l2 = new double[snaps];
+                volume1 = new double[snaps];
+                volume2 = new double[snaps];
+            }
+
 
             for (int i = 0; i < ens1lam1.size(); i++) {
                 e1l1[i] = ens1lam1.get(i);
