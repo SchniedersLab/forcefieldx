ab0ec8b10cf27e261d6fe4b5c43d4618d426b38a
anessler <aaron-nessler@uiowa.edu>
2021-09-28 16:06:18 -0500

    Improvements to PAC algorithm and handling of alternative space groups.

--- modules/algorithms/src/test/java/ffx/algorithms/groovy/ProgressiveAlignmentTest.java
+++ modules/algorithms/src/test/java/ffx/algorithms/groovy/ProgressiveAlignmentTest.java
@@ -67,13 +67,13 @@ public class ProgressiveAlignmentTest extends AlgorithmsTest {
     SuperposeCrystals SuperposeCrystals = new SuperposeCrystals(binding).run();
     algorithmsScript = SuperposeCrystals;
     assertEquals(0.0000, SuperposeCrystals.distMatrix[0][0], tolerance);
-    assertEquals(0.2016, SuperposeCrystals.distMatrix[0][1], tolerance);
-    assertEquals(0.2211, SuperposeCrystals.distMatrix[0][2], tolerance);
-    assertEquals(0.2016, SuperposeCrystals.distMatrix[1][0], tolerance);
+    assertEquals(0.2006, SuperposeCrystals.distMatrix[0][1], tolerance);
+    assertEquals(0.2611, SuperposeCrystals.distMatrix[0][2], tolerance);
+    assertEquals(0.2006, SuperposeCrystals.distMatrix[1][0], tolerance);
     assertEquals(0.0000, SuperposeCrystals.distMatrix[1][1], tolerance);
-    assertEquals(0.0865, SuperposeCrystals.distMatrix[1][2], tolerance);
-    assertEquals(0.2211, SuperposeCrystals.distMatrix[2][0], tolerance);
-    assertEquals(0.0865, SuperposeCrystals.distMatrix[2][1], tolerance);
+    assertEquals(0.1150, SuperposeCrystals.distMatrix[1][2], tolerance);
+    assertEquals(0.2611, SuperposeCrystals.distMatrix[2][0], tolerance);
+    assertEquals(0.1150, SuperposeCrystals.distMatrix[2][1], tolerance);
     assertEquals(0.0000, SuperposeCrystals.distMatrix[2][2], tolerance);
   }
 
@@ -90,13 +90,13 @@ public class ProgressiveAlignmentTest extends AlgorithmsTest {
     SuperposeCrystals SuperposeCrystals = new SuperposeCrystals(binding).run();
     algorithmsScript = SuperposeCrystals;
     assertEquals(0.0000, SuperposeCrystals.distMatrix[0][0], tolerance);
-    assertEquals(0.1901, SuperposeCrystals.distMatrix[0][1], tolerance);
-    assertEquals(0.2098, SuperposeCrystals.distMatrix[0][2], tolerance);
-    assertEquals(0.1901, SuperposeCrystals.distMatrix[1][0], tolerance);
+    assertEquals(0.1890, SuperposeCrystals.distMatrix[0][1], tolerance);
+    assertEquals(0.2509, SuperposeCrystals.distMatrix[0][2], tolerance);
+    assertEquals(0.1890, SuperposeCrystals.distMatrix[1][0], tolerance);
     assertEquals(0.0000, SuperposeCrystals.distMatrix[1][1], tolerance);
-    assertEquals(0.0863, SuperposeCrystals.distMatrix[1][2], tolerance);
-    assertEquals(0.2098, SuperposeCrystals.distMatrix[2][0], tolerance);
-    assertEquals(0.0863, SuperposeCrystals.distMatrix[2][1], tolerance);
+    assertEquals(0.1158, SuperposeCrystals.distMatrix[1][2], tolerance);
+    assertEquals(0.2509, SuperposeCrystals.distMatrix[2][0], tolerance);
+    assertEquals(0.1158, SuperposeCrystals.distMatrix[2][1], tolerance);
     assertEquals(0.0000, SuperposeCrystals.distMatrix[2][2], tolerance);
   }
 
