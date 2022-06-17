b22fe9ade97d17811dd307247606af7d0d5be4eb
Michael J. Schnieders <michael-schnieders@uiowa.edu>
2022-06-17 10:14:29 -0500

    Early progress on the GeneralizedKirkwoodTensor class

--- modules/numerics/src/main/java/ffx/numerics/multipole/GeneralizedKirkwoodTensor.java
+++ modules/numerics/src/main/java/ffx/numerics/multipole/GeneralizedKirkwoodTensor.java
@@ -50,25 +50,168 @@ import static org.apache.commons.math3.util.FastMath.sqrt;
  * @author Michael J. Schnieders
  * @since 1.0
  */
-public class GeneralizedKirkwoodTensor {
+public class GeneralizedKirkwoodTensor extends CoulombTensorGlobal {
+
+  /**
+   * Generalized Kirkwood constant.
+   */
+  private final double gc;
+
+  /**
+   * Homogeneous dielectric constant.
+   */
+  private final double Eh;
+
+  /**
+   * Solvent dielectric constant.
+   */
+  private final double Es;
+
+  /**
+   * Order of the tensor recursion (5th is needed for AMOEBA forces).
+   */
+  private final int order;
+
+  /**
+   * Multipole order (2nd is needed for AMOEBA forces).
+   */
+  private final int multipoleOrder;
+
+  /**
+   * The Kirkwood dielectric function for the given multipole order.
+   */
+  private final double c;
+
+  /**
+   * Compute the "source" terms for the recursion.
+   */
+  protected final double[] kirkwoodSource;
+
+  /**
+   *  Coefficients needed when taking derivatives of auxiliary functions.
+   */
+  private final double[][] anmc;
+
+  /**
+   * Born radius of atom i.
+   */
+  private double ai;
+
+  /**
+   * Born radius of atom j.
+   */
+  private double aj;
+
+  /**
+   * The GK term exp(-r2 / (gc * ai * aj)).
+   */
+  private double expTerm;
+
+  /**
+   * The GK effective separation distance.
+   */
+  private double f;
+
+  /**
+   *
+   * @param multipoleOrder The multipole order.
+   * @param order The number of derivatives to complete.
+   * @param gc Generalized Kirkwood constant.
+   * @param Eh Homogeneous dielectric constant.
+   * @param Es Solvent dielectric constant.
+   */
+  public GeneralizedKirkwoodTensor(int multipoleOrder, int order, double gc, double Eh, double Es) {
+    super(order);
+    this.multipoleOrder = multipoleOrder;
+    this.order = order;
+    this.gc = gc;
+    this.Eh = Eh;
+    this.Es = Es;
+
+    // Load the dielectric function
+    c = cn(multipoleOrder, Eh, Es);
+
+    // Auxiliary terms for Generalized Kirkwood (equivalent to Coulomb and Thole Screening).
+    kirkwoodSource = new double[order + 1];
+    for (int n = 0; n <= order; n++) {
+      kirkwoodSource[n] = c * pow(-1, n) * doubleFactorial(2 * n - 1);
+    }
+
+    anmc = new double[order + 1][];
+    for (int n = 0; n <= order; n++) {
+      anmc[n] = anmc(n);
+    }
+  }
+
+  public double selfEnergy(PolarizableMultipole polarizableMultipole, double a) {
+    double q2 = polarizableMultipole.q * polarizableMultipole.q;
+    double dx = polarizableMultipole.dx;
+    double dy = polarizableMultipole.dy;
+    double dz = polarizableMultipole.dz;
+    double dx2 = dx * dx;
+    double dy2 = dy * dy;
+    double dz2 = dz * dz;
+    double ux = polarizableMultipole.ux;
+    double uy = polarizableMultipole.uy;
+    double uz = polarizableMultipole.uz;
+    double qxy2 = polarizableMultipole.qxy * polarizableMultipole.qxy;
+    double qxz2 = polarizableMultipole.qxz * polarizableMultipole.qxz;
+    double qyz2 = polarizableMultipole.qyz * polarizableMultipole.qyz;
+    double qxx2 = polarizableMultipole.qxx * polarizableMultipole.qxx;
+    double qyy2 = polarizableMultipole.qyy * polarizableMultipole.qyy;
+    double qzz2 = polarizableMultipole.qzz * polarizableMultipole.qzz;
+
+    double a2 = a * a;
+    double a3 = a * a2;
+    double a5 = a2 * a3;
+
+    // Permanent self-energy
+    double e0 = cn(0, Eh, Es) * q2 / a;
+    double e1 = cn(1, Eh, Es) * (dx2 + dy2 + dz2) / a3;
+    double e2 = cn(2, Eh, Es) * (3.0 * (qxy2 + qxz2 + qyz2) + 6.0 * (qxx2 + qyy2 + qzz2)) / a5;
+
+    // Induced self-energy
+    double ei = cn(1, Eh, Es) * (dx * ux + dy * uy + dz * uz) / a3;
+
+    return 0.5 * (e0 + e1 + e2 + ei);
+  }
+
+  /**
+   * Set the separation vector.
+   *
+   * @param dx Separation along the X-axis.
+   * @param dy Separation along the Y-axis.
+   * @param dz Separation along the Z-axis.
+   * @param ai Born radius for Atom i.
+   * @param aj Born radius for Atom j.
+   */
+  protected void setR(double dx, double dy, double dz, double ai, double aj) {
+    setR(dx, dy, dz);
+    this.ai = ai;
+    this.aj = aj;
+    expTerm = exp(-r2 / (gc * ai * aj));
+    f = sqrt(r2 + ai * aj * expTerm);
+  }
+
+  /**
+   * Generate source terms for the Coulomb Challacombe et al. recursion.
+   *
+   * @param T000 Location to store the source terms.
+   */
+  protected void source(double[] T000) {
+    for (int n = 0; n <= order; n++) {
+      T000[n] = an0(n);
+    }
+  }
 
   /**
    * Compute the potential auxiliary function for a multipole of order n
    *
    * @param n Multipole order.
-   * @param r2 Separation distance squared.
-   * @param ai Born radius on atom i.
-   * @param aj Born radius on atom j.
-   * @param gc Generalized Kirkwood constant.
-   * @param Eh Homogeneous dielectric constant.
-   * @param Es Solvent dielectric constant.
    * @return The potential auxiliary function for a multipole of order n.
    */
-  public static double an0(
-      int n, double r2, double ai, double aj, double gc, double Eh, double Es) {
-    var expTerm = exp(-r2 / (gc * ai * aj));
-    var f = sqrt(r2 + ai * aj * expTerm);
-    return cn(n, Eh, Es) * pow(-1, n) * doubleFactorial(2 * n - 1) / pow(f, 2 * n + 1);
+  public double an0(int n) {
+    return kirkwoodSource[n] / pow(f, 2 * n + 1);
   }
 
   /**
@@ -76,25 +219,86 @@ public class GeneralizedKirkwoodTensor {
    *
    * @param n Multipole order.
    * @param m Mth potential gradient auxiliary function.
-   * @param r2 Separation distance squared.
-   * @param ai Born radius on atom i.
-   * @param aj Born radius on atom j.
-   * @param gc Generalized Kirkwood constant.
-   * @param Eh Homogeneous dielectric constant.
-   * @param Es Solvent dielectric constant.
    * @return Returns the mth potential gradient auxiliary function for a multipole of order n.
    */
-  public static double anm(
-      int n, int m, double r2, double ai, double aj, double gc, double Eh, double Es) {
+  public double anm(int n, int m) {
     if (m == 0) {
-      return an0(n, r2, ai, aj, gc, Eh, Es);
+      return T000[n];
     }
     var ret = 0.0;
-    var coef = anmc(n);
+    var coef = anmc[n];
     for (int i = 1; i <= m; i++) {
-      ret += coef[i - 1] * fn(i, r2, ai, aj, gc) * anm(n + 1, m - i, r2, ai, aj, gc, Eh, Es);
+      ret += coef[i - 1] * fn(i) * anm(n + 1, m - i);
     }
     return ret;
+  }
+
+  /**
+   * Compute the derivative with respect to a Born radius of the mth potential gradient auxiliary
+   * function for a multipole of order n.
+   *
+   * @param n Multipole order.
+   * @param m Mth potential gradient auxiliary function.
+   * @return Returns the derivative with respect to a Born radius of the mth potential gradient
+   *     auxiliary function for a multipole of order n.
+   */
+  public double bnm(int n, int m) {
+    if (m == 0) {
+      // return bn(0) * an0(n + 1);
+      return bn(0) * T000[n+1];
+    }
+    var ret = 0;
+    var coef = anmc(n);
+    for (int i = 1; i <= m; i++) {
+      ret += coef[i - 1] * bn(i) * anm(n + 1, m - i);
+      ret += coef[i - 1] * fn(i) * bnm(n + 1, m - i);
+    }
+    return ret;
+  }
+
+  /**
+   * Returns nth value of the function f, which are chain rule terms from differentiating zeroth
+   * order auxiliary functions (an0) with respect to x, y or z.
+   *
+   * @param n Multipole order.
+   * @return Returns the nth value of the function f.
+   */
+  private double fn(int n) {
+    switch (n) {
+      case 0:
+        return f;
+      case 1:
+        return 1.0 - expTerm / gc;
+      default:
+        var gcAiAj = gc * ai * aj;
+        var f2 = 2.0 * expTerm / (gc * gcAiAj);
+        var fr = -2.0 / gcAiAj;
+        return pow(fr, n - 2) * f2;
+    }
+  }
+
+  /**
+   * Returns nth value of the function b, which are chain rule terms from differentiating zeroth
+   * order auxiliary functions (an0) with respect to Ai or Aj.
+   *
+   * @param n Multipole order.
+   * @return Returns the nth value of the function f.
+   */
+  private double bn(int n) {
+    var gcAiAj = gc * ai * aj;
+    var ratio = -r2 / gcAiAj;
+    switch (n) {
+      case 0:
+        return 0.5 * expTerm * (1.0 - ratio);
+      case 1:
+        return -r2 * expTerm / (gcAiAj * gcAiAj);
+      default:
+        var b2 = 2.0 * expTerm / (gcAiAj * gcAiAj) * (-ratio - 1.0);
+        var br = 2.0 / (gcAiAj * ai * aj);
+        var f2 = 2.0 / (gc * gcAiAj) * expTerm;
+        var fr = -2.0 / (gcAiAj);
+        return (n - 2) * pow(fr, n - 3) * br * f2 + pow(fr, n - 2) * b2;
+    }
   }
 
   /**
@@ -104,120 +308,39 @@ public class GeneralizedKirkwoodTensor {
    * @return Returns coefficients needed when taking derivatives of auxiliary functions.
    */
   public static double[] anmc(int n) {
-    double[] ret = new double[n];
-    double[] prev = new double[n];
+    double[] ret = new double[n + 1];
     ret[0] = 1.0;
-    if (n == 1) {
-      return ret;
+    switch (n) {
+      case 0:
+        return ret;
+      case 1:
+        ret[1] = 1.0;
+        return ret;
+      default:
+        ret[1] = 1.0;
+        double[] prev = new double[n];
+        prev[0] = 1.0;
+        prev[1] = 1.0;
+        for (int i = 3; i <= n; i++) {
+          for (int j = 2; j <= i - 1; j++) {
+            ret[j - 1] = prev[j - 2] + prev[j - 1];
+          }
+          ret[i - 1] = 1.0;
+          arraycopy(ret, 0, prev, 0, i);
+        }
+        return ret;
     }
-    ret[1] = 1.0;
-    if (n == 2) {
-      return ret;
-    }
-    prev[0] = 1.0;
-    prev[1] = 1.0;
-    for (int i = 3; i <= n; i++) {
-      for (int j = 2; j <= i - 1; j++) {
-        ret[j - 1] = prev[j - 2] + prev[j - 1];
-      }
-      ret[i - 1] = 0.1e1;
-      arraycopy(ret, 0, prev, 0, i);
-    }
-    return ret;
-  }
-
-  /**
-   * Returns nth value of the function b, which are chain rule terms from differentiating zeroth
-   * order auxiliary functions (an0) with respect to Ai or Aj.
-   *
-   * @param n Multipole order.
-   * @param r2 Separation distance squared.
-   * @param Ai Born radius on atom i.
-   * @param Aj Born radius on atom j.
-   * @param gc Generalized Kirkwood constant.
-   * @return Returns the nth value of the function f.
-   */
-  public static double bn(int n, double r2, double Ai, double Aj, double gc) {
-    var gcAiAj = gc * Ai * Aj;
-    var ratio = -r2 / gcAiAj;
-    var expTerm = exp(ratio);
-    if (n == 0) {
-      return 0.5 * expTerm * (1.0 - ratio);
-    }
-    if (n == 1) {
-      return -r2 * expTerm / (gcAiAj * gcAiAj);
-    }
-    var b2 = 2.0 * expTerm / (gcAiAj * gcAiAj) * (-ratio - 1.0);
-    var br = 2.0 / (gcAiAj * Ai * Aj);
-    var f2 = 2.0 / (gc * gcAiAj) * expTerm;
-    var fr = -2.0 / (gcAiAj);
-    return (n - 2) * pow(fr, n - 3) * br * f2 + pow(fr, n - 2) * b2;
-  }
-
-  /**
-   * Compute the derivative with respect to a Born radius of the mth potential gradient auxiliary
-   * function for a multipole of order n.
-   *
-   * @param n Multipole order.
-   * @param m Mth potential gradient auxiliary function.
-   * @param r2 Separation distance squared.
-   * @param ai Born radius on atom i.
-   * @param aj Born radius on atom j.
-   * @param gc Generalized Kirkwood constant.
-   * @param Eh Homogeneous dielectric constant.
-   * @param Es Solvent dielectric constant.
-   * @return Returns the derivative with respect to a Born radius of the mth potential gradient
-   *     auxiliary function for a multipole of order n.
-   */
-  public static double bnm(
-      int n, int m, double r2, double ai, double aj, double gc, double Eh, double Es) {
-    if (m == 0) {
-      return bn(0, r2, ai, aj, gc) * an0(n + 1, r2, ai, aj, gc, Eh, Es);
-    }
-    var ret = 0;
-    var coef = anmc(n);
-    for (int i = 1; i <= m; i++) {
-      ret += coef[i - 1] * bn(i, r2, ai, aj, gc) * anm(n + 1, m - i, r2, ai, aj, gc, Eh, Es);
-      ret += coef[i - 1] * fn(i, r2, ai, aj, gc) * bnm(n + 1, m - i, r2, ai, aj, gc, Eh, Es);
-    }
-    return ret;
   }
 
   /**
    * Compute the Kirkwood dielectric function for a multipole of order n.
    *
    * @param n Multipole order.
-   * @param Eh Homogeneous dieletric.
-   * @param Es Solvent dieletric.
+   * @param Eh Homogeneous dielectric.
+   * @param Es Solvent dielectric.
    * @return Returns (n+1)*(Eh-Es)/((n+1)*Es + n*Eh))
    */
   public static double cn(int n, double Eh, double Es) {
     return (n + 1) * (Eh - Es) / ((n + 1) * Es + n * Eh);
-  }
-
-  /**
-   * Returns nth value of the function f, which are chain rule terms from differentiating zeroth
-   * order auxiliary functions (an0) with respect to x, y or z.
-   *
-   * @param n Multipole order.
-   * @param r2 Separation distance squared.
-   * @param Ai Born radius on atom i.
-   * @param Aj Born radius on atom j.
-   * @param gc Generalized Kirkwood constant.
-   * @return Returns the nth value of the function f.
-   */
-  public static double fn(int n, double r2, double Ai, double Aj, double gc) {
-    var gcAiAj = gc * Ai * Aj;
-    var ratio = -r2 / gcAiAj;
-    var expTerm = exp(ratio);
-    if (n == 0) {
-      return sqrt(r2 + Ai * Aj * expTerm);
-    }
-    if (n == 1) {
-      return 1.0 - expTerm / gc;
-    }
-    var f2 = 2.0 * expTerm / (gc * gcAiAj);
-    var fr = -2.0 / gcAiAj;
-    return pow(fr, n - 2) * f2;
   }
 }
