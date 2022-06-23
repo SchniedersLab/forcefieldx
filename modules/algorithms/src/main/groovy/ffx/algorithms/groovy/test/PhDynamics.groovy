69ff7222e8c28fddeb5cd3bb2b2a70030f55a9d1
mattsperanza <mattsperanza@outlook.com>
2022-06-23 14:42:30 -0500

    fixed if 1

--- modules/algorithms/src/main/groovy/ffx/algorithms/groovy/test/PhDynamics.groovy
+++ modules/algorithms/src/main/groovy/ffx/algorithms/groovy/test/PhDynamics.groovy
@@ -238,7 +238,7 @@ class PhDynamics extends AlgorithmsScript {
         bwr.write("")
         String data = br.readLine()
         while(data != null) {
-          if (data.substring(0,3).contains("[" + world.rank() + "]")) {
+          if (data.substring(0,4).contains("[" + world.rank() + "]")) {
             wr.write(data + "\n")
           }
           data = br.readLine()
