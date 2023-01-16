ab9e172ceaa2a7c195df78c55923d2cbf10541fe
Michael J. Schnieders <michael-schnieders@uiowa.edu>
2023-01-16 14:39:49 -0600

    Minor change to TornadoVM DFT test code

--- modules/numerics/src/main/java/ffx/numerics/tornado/FFXTornado.java
+++ modules/numerics/src/main/java/ffx/numerics/tornado/FFXTornado.java
@@ -37,8 +37,6 @@
 // ******************************************************************************
 package ffx.numerics.tornado;
 
-import static java.lang.String.format;
-
 import java.util.logging.Logger;
 import uk.ac.manchester.tornado.api.TornadoDriver;
 import uk.ac.manchester.tornado.api.TornadoTargetDevice;
@@ -96,6 +94,7 @@ public class FFXTornado {
       for (int deviceIndex = 0; deviceIndex < driver.getDeviceCount(); deviceIndex++) {
         if (n == deviceID) {
           TornadoRuntime.setProperty("devices", driverIndex + ":" + deviceIndex);
+          String name = driver.getName();
           return TornadoRuntime.createDevice("opencl", driverIndex, deviceIndex);
         }
         n++;
@@ -140,13 +139,12 @@ public class FFXTornado {
     // tornadoTargetDevice.getDeviceGlobalMemorySize() / 1024 / 1024));
     //        logger.info(format(" Local Memory:        %6d KB",
     // tornadoTargetDevice.getDeviceLocalMemorySize() / 1024));
-    System.out.println(format("\n Device Name:         %s", tornadoTargetDevice.getDeviceName()));
-    System.out.println(format(" Compute Units:       %s", tornadoTargetDevice.getDeviceMaxComputeUnits()));
-    System.out.println(format(" Max Work Item Sizes: [%d, %d, %d]",
-            workItemSize[0], workItemSize[1], workItemSize[2]));
-    System.out.println(format(" Clock Frequency:     %6d Ghz", tornadoTargetDevice.getDeviceMaxClockFrequency()));
-    System.out.println(format(" Global Memory:       %6d MB",
-            tornadoTargetDevice.getDeviceGlobalMemorySize() / 1024 / 1024));
-    System.out.println(format(" Local Memory:        %6d KB", tornadoTargetDevice.getDeviceLocalMemorySize() / 1024));
+    System.out.printf("\n Device Name:         %s%n", tornadoTargetDevice.getDeviceName());
+    System.out.printf(" Backend:             %s%n", device.getTornadoVMBackend().name());
+    System.out.printf(" Compute Units:       %s%n", tornadoTargetDevice.getDeviceMaxComputeUnits());
+    System.out.printf(" Max Work Item Sizes: [%d, %d, %d]%n", workItemSize[0], workItemSize[1], workItemSize[2]);
+    System.out.printf(" Clock Frequency:     %6d Ghz%n", tornadoTargetDevice.getDeviceMaxClockFrequency());
+    System.out.printf(" Global Memory:       %6d MB%n", tornadoTargetDevice.getDeviceGlobalMemorySize() / 1024 / 1024);
+    System.out.printf(" Local Memory:        %6d KB%n", tornadoTargetDevice.getDeviceLocalMemorySize() / 1024);
   }
 }
