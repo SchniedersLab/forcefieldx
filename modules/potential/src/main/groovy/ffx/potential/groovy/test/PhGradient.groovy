37d4373e42cfbd1ebb6b9d7e26a44d865ae1ca06
Michael J. Schnieders <michael-schnieders@uiowa.edu>
2023-01-22 12:02:56 -0600

    Bump the Copyright to 2023

--- modules/potential/src/main/groovy/ffx/potential/groovy/test/PhGradient.groovy
+++ modules/potential/src/main/groovy/ffx/potential/groovy/test/PhGradient.groovy
@@ -2,7 +2,7 @@
 //
 // Title:       Force Field X.
 // Description: Force Field X - Software for Molecular Biophysics.
-// Copyright:   Copyright (c) Michael J. Schnieders 2001-2022.
+// Copyright:   Copyright (c) Michael J. Schnieders 2001-2023.
 //
 // This file is part of Force Field X.
 //
