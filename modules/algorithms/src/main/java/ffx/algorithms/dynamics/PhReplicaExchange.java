c077da7750a43788d19b9b3a14352ef8cc00e6a4
mattsperanza <mattsperanza@outlook.com>
2023-06-26 10:31:49 -0500

    Fix .arc print timing and revert changes made to Thermodynamic and ThermodynamicOptions

--- modules/algorithms/src/main/java/ffx/algorithms/dynamics/PhReplicaExchange.java
+++ modules/algorithms/src/main/java/ffx/algorithms/dynamics/PhReplicaExchange.java
@@ -691,11 +691,10 @@ public class PhReplicaExchange implements Terminatable {
     x = openMM.getCoordinates();
     replica.setCoordinates(x);
 
-    double forceWriteInterval = titrSteps * 0.001;
+    double forceWriteInterval = titrSteps - 1;
     replica.dynamic(titrSteps, timeStep, printInterval, forceWriteInterval, temp, initVelocities, dyn);
 
-    // Update this ranks' parameter array to be consistent with the dynamics.
-
+    // Update this ranks' parameter array to be consistent with the dynamics.lea
     myParameters[0] = pHScale[i];
     myParameters[2] = extendedSystem.getBiasEnergy();
 
