e83b1c0857993f8f1a34cfecf68a26c8831c2df3
Jacob M Miller <jmiller99@engr.uiowa.edu>
2024-02-23 17:03:22 -0600

    Added all FF params to forcefield object but AtomType.

--- modules/potential/src/main/groovy/ffx/potential/groovy/test/ReadXML.groovy
+++ modules/potential/src/main/groovy/ffx/potential/groovy/test/ReadXML.groovy
@@ -67,6 +67,7 @@ import picocli.CommandLine.Parameters
 import javax.xml.parsers.DocumentBuilder
 import javax.xml.parsers.DocumentBuilderFactory
 
+import static java.lang.Integer.toString
 import static java.lang.String.format
 import static ffx.potential.bonded.Atom.ElementSymbol;
 import static org.apache.commons.math3.util.FastMath.abs;
@@ -84,18 +85,21 @@ import static java.lang.Integer.parseInt;
  * <br>
  * ffxc test.ReadXML &lt;filename&gt;
  */
-@Command(description = " Convert XML file to Patch.", name = "ffxc test.ReadXML")
+@Command(description = " Read OpenMM XML file.", name = "ffxc test.ReadXML")
 class ReadXML extends PotentialScript {
 
     /**
      * The final argument(s) should be two filenames.
      */
     @Parameters(arity = "1", paramLabel = "file",
-            description = 'XML file to be converted to Patch.')
+            description = 'XML FF file to be read.')
     List<String> filenames = null
 
-//    final double ANGperNM = 10.0
-//    final double KJperKCal = 4.184
+    final double ANGperNM = 10.0;
+    final double KJperKCal = 4.184;
+
+    LinkedHashMap<String, Integer> atomClassMap
+    int atomClassNum = 0
 
     /**
      * Execute the script.
@@ -108,6 +112,24 @@ class ReadXML extends PotentialScript {
             return this
         }
 
+        CompositeConfiguration properties = Keyword.loadProperties(null);
+        properties.addProperty("renumberPatch", "FALSE")
+        properties.addProperty("FORCEFIELD", "AMBER_1999_SB")
+        properties.addProperty("VDWTYPE", "LENNARD-JONES")
+        properties.addProperty("RADIUSRULE", "ARITHMETIC")
+        properties.addProperty("RADIUSTYPE", "R-MIN")
+//        properties.addProperty("RADIUSTYPE", "SIGMA")
+        properties.addProperty("RADIUSSIZE", "RADIUS")
+//        properties.addProperty("RADIUSSIZE", "DIAMETER")
+        properties.addProperty("EPSILONRULE", "GEOMETRIC")
+        properties.addProperty("VDW-14-SCALE", "2.0")
+//        properties.addProperty("VDW-14-SCALE", "0.500000")
+        properties.addProperty("CHG-14-SCALE", "1.2")
+        properties.addProperty("ELECTRIC", "332.0522173") //was 332.0716
+        properties.addProperty("DIELECTRIC", "1.0")
+        ForceField forceField = new ForceField(properties);
+
+        atomClassMap = new LinkedHashMap<>()
         File inputFile = new File(filenames[0])
         String fileName = FilenameUtils.removeExtension(inputFile.getName())
         DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance()
@@ -129,10 +151,30 @@ class ReadXML extends PotentialScript {
                     case "AtomTypes":
                         NodeList types = child.getChildNodes()
                         logger.info(format("AtomTypes nodes: %d",types.length))
-//                        int counter = 0
+//                        int atomClassNum = 0
                         for (Node atom : types) {
                             if (atom.getNodeName() == "Type") {
                                 logger.info(format("%s %s %s %s",atom.getAttribute("name"),atom.getAttribute("class"),atom.getAttribute("element"),atom.getAttribute("mass")))
+
+                                int atomType = parseInt(atom.getAttribute("name"))
+                                String className = atom.getAttribute("class")
+                                String element = atom.getAttribute("element")
+                                double mass = parseDouble(atom.getAttribute("mass"))
+
+                                if (!atomClassMap.containsKey(className)) {
+                                    atomClassMap.put(className, atomClassNum)
+                                    logger.info(format("KEY: %s     VALUE: %d",className,atomClassNum))
+                                    atomClassNum++
+                                }
+
+                                int atomClass = atomClassMap.get(className)
+                                int atomicNumber = ElementSymbol.valueOf(element).ordinal()+1
+//                                logger.info(format("ELEMENT: %s %d",element,ElementSymbol.valueOf(element).ordinal()+1))
+
+                                //TODO: get valence -> go thru residues and count up the number of bonds for each atom
+                                // type.. will likely now have to add the atomtype to the forcefield outside of this loop
+//                                forceField.addForceFieldType(new AtomType(atomType, atomClass, element, className, atomicNumber, mass, VALENCE))
+
                             } else if (atom.hasAttributes()) {
                                 logger.info("CHECK")
                             }
@@ -170,6 +212,28 @@ class ReadXML extends PotentialScript {
                         for (Node bond : bonds) {
                             if (bond.getNodeName() == "Bond") {
                                 logger.info(format("%s %s %s %s",bond.getAttribute("class1"),bond.getAttribute("class2"),bond.getAttribute("length"),bond.getAttribute("k")))
+                                String class1 = bond.getAttribute("class1")
+                                String class2 = bond.getAttribute("class2")
+                                String bondLength = bond.getAttribute("length")
+                                String k = bond.getAttribute("k")
+//                                int[] classes = {atomClassMap.get(class1); atomClassMap.get(class2)}
+//                                logger.info(format("BOND %s - %s (%s - %s",class1,class2,classes[0],classes[1]))
+                                if (!atomClassMap.containsKey(class1)) {
+                                    atomClassMap.put(class1, atomClassNum)
+                                    logger.info(format("KEY: %s     VALUE: %d",class1,atomClassNum))
+                                    atomClassNum++
+                                }
+                                if (!atomClassMap.containsKey(class2)) {
+                                    atomClassMap.put(class2, atomClassNum)
+                                    logger.info(format("KEY: %s     VALUE: %d",class2,atomClassNum))
+                                    atomClassNum++
+                                }
+
+                                int[] classes = new int[2]
+                                classes[0] = atomClassMap.get(class1)
+                                classes[1] = atomClassMap.get(class2)
+//                                logger.info(format("BOND %s - %s (%s - %s",class1,class2,class1Int,class2Int))
+                                forceField.addForceFieldType(new BondType(classes, parseDouble(k), parseDouble(bondLength)))
                             } else if (bond.hasAttributes()) {
                                 logger.info("CHECK")
                             }
@@ -182,7 +246,38 @@ class ReadXML extends PotentialScript {
 
                         for (Node angle : angles) {
                             if (angle.getNodeName() == "Angle") {
-                                logger.info(format("%s %s %s %s %s",angle.getAttribute("class1"),angle.getAttribute("class2"),angle.getAttribute("class3"),angle.getAttribute("angle"),angle.getAttribute("k")))
+                                logger.info(format("%s %s %s %s %s",angle.getAttribute("class1"),angle.getAttribute("class2"),
+                                        angle.getAttribute("class3"),angle.getAttribute("angle"),angle.getAttribute("k")))
+                                String class1 = angle.getAttribute("class1")
+                                String class2 = angle.getAttribute("class2")
+                                String class3 = angle.getAttribute("class3")
+                                String angleStr = angle.getAttribute("angle")
+                                String k = angle.getAttribute("k")
+
+                                if (!atomClassMap.containsKey(class1)) {
+                                    atomClassMap.put(class1, atomClassNum)
+                                    logger.info(format("KEY: %s     VALUE: %d",class1,atomClassNum))
+                                    atomClassNum++
+                                }
+                                if (!atomClassMap.containsKey(class2)) {
+                                    atomClassMap.put(class2, atomClassNum)
+                                    logger.info(format("KEY: %s     VALUE: %d",class2,atomClassNum))
+                                    atomClassNum++
+                                }
+                                if (!atomClassMap.containsKey(class3)) {
+                                    atomClassMap.put(class3, atomClassNum)
+                                    logger.info(format("KEY: %s     VALUE: %d",class3,atomClassNum))
+                                    atomClassNum++
+                                }
+
+                                int[] classes = new int[3]
+                                classes[0] = atomClassMap.get(class1)
+                                classes[1] = atomClassMap.get(class2)
+                                classes[2] = atomClassMap.get(class3)
+                                double[] angleArr = new double[1]
+                                angleArr[0] = parseDouble(angleStr) * 180.0 / PI  // convert to degrees
+
+                                forceField.addForceFieldType(new AngleType(classes, parseDouble(k), angleArr))
                             } else if (angle.hasAttributes()) {
                                 logger.info("CHECK")
                             }
@@ -193,8 +288,52 @@ class ReadXML extends PotentialScript {
                         NodeList torsions = child.getChildNodes()
                         logger.info(format("PeriodicTorsionForce nodes: %d",torsions.length))
 
-                        // TODO: Has some with periodicity 1 and 2 -> test for this
-                        // TODO: also Node names: Proper & Improper
+                        for (Node torsion : torsions) {
+                            if (torsion.getNodeName() == "Proper" || torsion.getNodeName() == "Improper") {
+                                logger.info(format("%s %s %s %s %s %s %s",torsion.getAttribute("class1"),
+                                        torsion.getAttribute("class2"),torsion.getAttribute("class3"),
+                                        torsion.getAttribute("class4"),torsion.getAttribute("periodicity1"),
+                                        torsion.getAttribute("phase1"),torsion.getAttribute("k1")))
+                                if (torsion.hasAttribute("periodicity2")) {
+                                    logger.info(format("%s %s %s",torsion.getAttribute("periodicity2"),torsion.getAttribute("phase2"),torsion.getAttribute("k2")))
+                                    if(torsion.hasAttribute("periodicity3")) {
+                                        logger.info(format("%s %s %s",torsion.getAttribute("periodicity3"),torsion.getAttribute("phase3"),torsion.getAttribute("k3")))
+                                        if(torsion.hasAttribute("periodicity4")) {
+                                            logger.info(format("%s %s %s",torsion.getAttribute("periodicity4"),torsion.getAttribute("phase4"),torsion.getAttribute("k4")))
+                                        }
+                                    }
+                                }
+
+                                int numTerms = (torsion.getAttributes().length - 4) / 3  // get number of amplitudes/phases/periodicities
+                                int[] classes = new int[4]
+                                int[] periods = new int[numTerms]
+                                double[] phases = new double[numTerms]
+                                double[] amplitudes = new double[numTerms]
+                                for (int i = 1; i <= 4; i++) {
+                                    if (!atomClassMap.containsKey(torsion.getAttribute("class"+toString(i)))) {
+                                        atomClassMap.put(torsion.getAttribute("class"+toString(i)), atomClassNum)
+                                        logger.info(format("KEY: %s     VALUE: %d",torsion.getAttribute("class"+toString(i)),atomClassNum))
+                                        atomClassNum++
+                                    }
+                                    classes[i-1] = atomClassMap.get(torsion.getAttribute("class"+toString(i)))
+
+                                    if (torsion.hasAttribute("periodicity"+toString(i))) { // could replace with i < numTerms
+                                        periods[i-1] = parseInt(torsion.getAttribute("periodicity"+toString(i)))
+                                        phases[i-1] = parseDouble(torsion.getAttribute("phase"+toString(i))) * 180.0 / PI  // convert to degrees
+                                        amplitudes[i-1] = parseDouble(torsion.getAttribute("k"+toString(i)))
+                                    }
+                                }
+
+                                if (torsion.getNodeName() == "Proper") {
+                                    forceField.addForceFieldType(new TorsionType(classes, amplitudes, phases, periods, TorsionType.TorsionMode.NORMAL))
+                                } else {
+                                    forceField.addForceFieldType(new TorsionType(classes, amplitudes, phases, periods, TorsionType.TorsionMode.IMPROPER))
+                                }
+                            }
+                            else if (torsion.hasAttributes()) {
+                                logger.info("CHECK")
+                            }
+                        }
                         break
 
                     case "NonbondedForce":
@@ -204,6 +343,14 @@ class ReadXML extends PotentialScript {
                         for (Node nbF : nbForces) {
                             if (nbF.getNodeName() == "Atom") {
                                 logger.info(format("%s %s %s %s",nbF.getAttribute("type"),nbF.getAttribute("charge"),nbF.getAttribute("sigma"),nbF.getAttribute("epsilon")))
+                                int atomType = parseInt(nbF.getAttribute("type"))
+                                double q = parseDouble(nbF.getAttribute("charge")) // in proton units
+                                double sigma = parseDouble(nbF.getAttribute("sigma")) * ANGperNM  // nm to Ang
+                                double eps = parseDouble(nbF.getAttribute("epsilon")) / KJperKCal  // kJ/mol? to KCal/mol
+
+                                forceField.addForceFieldType(new ChargeType(atomType, q))
+                                forceField.addForceFieldType(new VDWType(atomType, sigma, eps, -1.0))
+
                             } else if (nbF.hasAttributes()) {
                                 logger.info("CHECK")
                             }
