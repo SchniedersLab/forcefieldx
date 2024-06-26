40dfaa03a286de02bc0fbe20d8e88622971a3cb8
Rose Gogal <rgogal@engr.uiowa.edu>
2024-06-26 15:37:32 -0500

    Try new method of fmod for gk2023 using potential energy

--- modules/potential/src/main/java/ffx/potential/parameters/TitrationUtils.java
+++ modules/potential/src/main/java/ffx/potential/parameters/TitrationUtils.java
@@ -1872,15 +1872,15 @@ public class TitrationUtils {
    */
   public enum Titration {
 
-    ASHtoASP(3.94, -70.35, -35.35,-24.39, -3.15, 12.730, -107.430, 166.369, 0.0, AminoAcid3.ASH, AminoAcid3.ASP),
+    ASHtoASP(3.94, -70.35, -35.35,-43.16, -3.15, 12.730, -107.430, 166.369, 0.0, AminoAcid3.ASH, AminoAcid3.ASP),
     ASH1toASH2(Double.NaN, 0.00,0,0,0, 0.0, -35.305, 35.305, 0.0, AminoAcid3.ASH, AminoAcid3.ASH),
-    GLHtoGLU(4.25, -81.90, -39.71, -36.54, -7.3,28.024, -131.270, 189.980, 0.0,  AminoAcid3.GLH, AminoAcid3.GLU),
+    GLHtoGLU(4.25, -81.90, -39.71, -54.40, -7.3,28.024, -131.270, 189.980, 0.0,  AminoAcid3.GLH, AminoAcid3.GLU),
     GLH1toGLH2(Double.NaN, 0,0,0,0.00, 0.0, -29.395, 29.395, 0.0, AminoAcid3.GLH, AminoAcid3.GLH),
-    LYStoLYD(10.40, 41.35, 20.0, 41.31, 19.7,6.752, -78.804, 26.894, 0.0, AminoAcid3.LYS, AminoAcid3.LYD),
+    LYStoLYD(10.40, 41.35, 20.0, 20.90, 19.7,6.752, -78.804, 26.894, 0.0, AminoAcid3.LYS, AminoAcid3.LYD),
     //TYRtoTYD(10.07, 34.961, 0.0, AminoAcidUtils.AminoAcid3.TYR, AminoAcidUtils.AminoAcid3.TYD),
-    CYStoCYD(8.55, -53.1, 0.0, -37.70, -6.75,44.247, -183.990, 226.710, 0.0, AminoAcid3.CYS, AminoAcid3.CYD), //HE2 is the proton that is lost
-    HIStoHID(7.00, 40.20, 20.92, 15.65, 18.83, 0.0, -64.317, 30.350, 0.0,  AminoAcid3.HIS, AminoAcid3.HID), //HD1 is the proton that is lost
-    HIStoHIE(6.60, 37.44, 18.14, 29.4, 17.95,0.0, -62.931, 32.000, 0.0,AminoAcid3.HIS, AminoAcid3.HIE),
+    CYStoCYD(8.55, -53.1, 0.0, -33.12, -6.75,44.247, -183.990, 226.710, 0.0, AminoAcid3.CYS, AminoAcid3.CYD), //HE2 is the proton that is lost
+    HIStoHID(7.00, 40.20, 20.92, 38.16, 18.83, 0.0, -64.317, 30.350, 0.0,  AminoAcid3.HIS, AminoAcid3.HID), //HD1 is the proton that is lost
+    HIStoHIE(6.60, 37.44, 18.14, 36.43, 17.95,0.0, -62.931, 32.000, 0.0,AminoAcid3.HIS, AminoAcid3.HIE),
     HIDtoHIE(Double.NaN, 0.00, 0.0, 0.0, 0.0,0.0, -36.830, 34.325, 0.0,AminoAcid3.HID, AminoAcid3.HIE);
     //TerminalNH3toNH2(8.23, 0.0, 00.00, AminoAcidUtils.AminoAcid3.UNK, AminoAcidUtils.AminoAcid3.UNK),
     //TerminalCOOHtoCOO(3.55, 0.0, 00.00, AminoAcidUtils.AminoAcid3.UNK, AminoAcidUtils.AminoAcid3.UNK);
