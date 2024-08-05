a1d472e60f93d3e540a3423f78094fe30719fffb
Michael J. Schnieders <michael-schnieders@uiowa.edu>
2024-08-05 17:00:36 -0500

    Added the vdw-lambda-end flag to specify when the lambda vdW path should finish (where the vdW energy is full strength

--- modules/potential/src/main/java/ffx/potential/nonbonded/VanDerWaals.java
+++ modules/potential/src/main/java/ffx/potential/nonbonded/VanDerWaals.java
@@ -187,6 +187,10 @@ public class VanDerWaals implements MaskingInterface, LambdaInterface {
    */
   private double lambda = 1.0;
   /**
+   * The vdW energy is fully turned on when this value is reached.
+   */
+  private double vdwLambdaEnd = 1.0;
+  /**
    * Exponent on lambda (beta).
    */
   private double vdwLambdaExponent = 3.0;
@@ -342,6 +346,14 @@ public class VanDerWaals implements MaskingInterface, LambdaInterface {
             vdwLambdaExponent));
         vdwLambdaExponent = 3.0;
       }
+
+      vdwLambdaEnd = forceField.getDouble("VDW_LAMBDA_END", 1.0);
+      if (vdwLambdaEnd < 0.0 || vdwLambdaEnd > 1.0) {
+        logger.warning(format(
+            " Invalid value %8.3g for vdw-lambda-end; must be between 0 and 1. Resetting to 1.", vdwLambdaEnd));
+        vdwLambdaEnd = 1.0;
+      }
+
       intermolecularSoftcore = forceField.getBoolean("INTERMOLECULAR_SOFTCORE", false);
       intramolecularSoftcore = forceField.getBoolean("INTRAMOLECULAR_SOFTCORE", false);
     } else {
@@ -577,7 +589,14 @@ public class VanDerWaals implements MaskingInterface, LambdaInterface {
       return;
     }
 
+    // Scale lambda if vdwLambdaEnd is less than 1.0.
+    if (lambda > vdwLambdaEnd) {
+      lambda = 1.0;
+    } else {
+      lambda = lambda / vdwLambdaEnd;
+    }
     this.lambda = lambda;
+
     // Softcore constant in vdW denominator sc1 = alpha * (1-L)^2
     sc1 = vdwLambdaAlpha * (1.0 - lambda) * (1.0 - lambda);
     // d(sc1)/dL = -2 * alpha * (1-L)
