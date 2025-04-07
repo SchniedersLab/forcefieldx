1f221a070d23a18d0b8dc27a86d6e5a263958f51
Rose Gogal <rose-gogal@uiowa.edu>
2025-04-07 11:50:23 -0500

    Update HIS fmod to ensure correct ratio of HIE/HID

--- modules/potential/src/main/java/ffx/potential/parameters/TitrationUtils.java
+++ modules/potential/src/main/java/ffx/potential/parameters/TitrationUtils.java
@@ -1877,8 +1877,8 @@ public class TitrationUtils {
     LYStoLYD(10.40, 41.35, 20.0, 41.31, 19.80,6.752, -78.804, 26.894, 0.0, AminoAcid3.LYS, AminoAcid3.LYD),
     //TYRtoTYD(10.07, 34.961, 0.0, AminoAcidUtils.AminoAcid3.TYR, AminoAcidUtils.AminoAcid3.TYD),
     CYStoCYD(8.55, -53.1, 0.0, -33.12, -6.75,44.247, -183.990, 226.710, 0.0, AminoAcid3.CYS, AminoAcid3.CYD), //HE2 is the proton that is lost
-    HIStoHID(7.00, 40.20, 20.92, 26.30, 25.0, 0.0, -64.317, 30.350, 0.0,  AminoAcid3.HIS, AminoAcid3.HID), //HD1 is the proton that is lost
-    HIStoHIE(6.60, 37.44, 18.14, 40.43, 22.9,0.0, -62.931, 32.000, 0.0,AminoAcid3.HIS, AminoAcid3.HIE),
+    HIStoHID(7.00, 40.20, 16.55, 26.30, 25.0, 0.0, -64.317, 30.350, 0.0,  AminoAcid3.HIS, AminoAcid3.HID), //HD1 is the proton that is lost
+    HIStoHIE(6.60, 37.44, 17.41, 40.43, 22.9,0.0, -62.931, 32.000, 0.0,AminoAcid3.HIS, AminoAcid3.HIE),
     HIDtoHIE(Double.NaN, 0.00, 0.0, 0.0, 0.0,0.0, -36.830, 34.325, 0.0,AminoAcid3.HID, AminoAcid3.HIE);
     //TerminalNH3toNH2(8.23, 0.0, 00.00, AminoAcidUtils.AminoAcid3.UNK, AminoAcidUtils.AminoAcid3.UNK),
     //TerminalCOOHtoCOO(3.55, 0.0, 00.00, AminoAcidUtils.AminoAcid3.UNK, AminoAcidUtils.AminoAcid3.UNK);
