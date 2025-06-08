cc47cd101ab754e8a6c6d03320f9ceb15f1cd424
Michael J. Schnieders <michael-schnieders@uiowa.edu>
2025-06-08 10:40:03 -0500

    Update to Groovy 5.0.0 Beta 1

--- modules/utilities/src/main/java/ffx/utilities/Constants.java
+++ modules/utilities/src/main/java/ffx/utilities/Constants.java
@@ -103,7 +103,7 @@ public class Constants {
    */
   public static final int K_CD_SI = 683;
   /**
-   * Constant <code>LITERS_PER_CUBIC_ANGSTROM=1E-30</code>
+   * Constant <code>LITERS_PER_CUBIC_ANGSTROM=1E-27</code>
    */
   public static final double LITERS_PER_CUBIC_ANGSTROM = 1E-27;
   /**
